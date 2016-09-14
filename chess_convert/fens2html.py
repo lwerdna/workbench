4751a34e581c91ea2bbc22b59430e9a2dc19b93c
Andrew Lamoureux <andrew@vector35.com>
2016-09-14 00:49:47 -0400

    chess_convert: fens2html generates playable pages

--- chess_convert/fens2html.py
+++ chess_convert/fens2html.py
@@ -9,6 +9,10 @@ def getFen(filename):
 	fp.close
 	return fen
 
+def problemToFname(problem):
+	(book,chap,prob,fen) = problem
+	return 'problem_%02d_%02d_%02d.html' % (book,chap,prob)
+
 problems=[]
 for (dirpath, dirnames, filenames) in os.walk('./fens'):
 	for filename in filenames:
@@ -22,36 +26,39 @@ for (dirpath, dirnames, filenames) in os.walk('./fens'):
 problems = sorted(problems, key=lambda x: x[0]*10000+x[1]*100+x[2])
 
 # write html
-fp = open('output.html', 'w')
-fp.write('<html>\n')
-fp.write('<head>\n')
-fp.write('  <meta charset="utf-8" />\n')
-fp.write('  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />\n')
-fp.write('  <title>Multiple Boards Example</title>\n')
-fp.write('  <base href="http://chessboardjs.com/" />\n')
-fp.write('\n')
-fp.write('  <link rel="stylesheet" href="css/chessboard.css" />\n')
-fp.write('</head>\n')
-fp.write('<body>\n')
-for problem in problems:
-	(book,chap,prob,fen) = problem
-	id_ = 'board_%d_%d_%d' % (book,chap,prob)
-	fp.write('<h3>Chapter %d Problem %d</h3>\n' % (chap,prob))
-	fp.write('<div id="%s" style="width: 400px"></div>\n' % id_)
-fp.write('<script src="js/json3.min.js"></script>\n')
-fp.write('<script src="js/jquery-1.10.1.min.js"></script>\n')
-fp.write('<script src="js/chessboard.js"></script>\n')
-fp.write('<script>\n')
-fp.write('var init = function() {\n')
-for problem in problems:
-	(book,chap,prob,fen) = problem
-	id_ = 'board_%d_%d_%d' % (book,chap,prob)
-	fp.write('var %s = ChessBoard(\'%s\', \'%s\');\n' % (id_, id_, fen))
-fp.write('}; // end init()\n')
-fp.write('$(document).ready(init);\n')
-fp.write('</script>\n')
-fp.write('</body>\n')
-fp.write('</html>\n')
-fp.close()
+fname = 'board_%d_%d_%d' % (book,chap,prob)
 
+for (i,problem) in enumerate(problems):
+	(book,chap,prob,fen) = problem
+	html_name = problemToFname(problem)
+	fp = open(html_name, 'w')
+	fp.write('<html>\n')
+	fp.write('	<head>\n')
+	fp.write('		<link rel="stylesheet" href="css/chessboard-0.3.0.min.css" />\n')
+	fp.write('		<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>\n')
+	fp.write('		<script src="js/chess.min.js"></script>\n')
+	fp.write('		<script src="js/chessboard-0.3.0.min.js"></script>\n')
+	fp.write('		<script src="js/puzzle.js"></script>\n')
+	fp.write('	</head>\n')
+	fp.write('	<body>\n')
+	fp.write('		<div id="board" style="width: 400px"></div>\n')
+	fp.write('		<script>\n')
+	fp.write('			var fen = \'%s\';\n' % fen)
+	fp.write('			var game = engineGame(fen);\n')
+	fp.write('		</script>\n')
+	fp.write('\n')
+	fp.write('<p>Yusupov Book %d Chapter %d Problem %d</p>\n' % (book,chap,prob))
+	if(fen.split(' ')[1]=='w'):
+		fp.write('<p>White to play!</p>\n')
+	else:
+		fp.write('<p>Black to play!</p>\n')
+	fp.write('<p>')
+	if i != 0:
+		fp.write('<a href="%s">&lt;previous</a>&nbsp;' % problemToFname(problems[i-1]))
+	if i != len(problems)-1:
+		fp.write('<a href="%s">next&gt;</a>' % problemToFname(problems[i+1]))
+	fp.write('</p>\n');
+	fp.write('</body>\n');
+	fp.write('</html>\n');
+	fp.close();
 	
