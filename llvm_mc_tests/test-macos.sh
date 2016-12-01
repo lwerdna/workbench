b67cd4c6002a219f6a336bc927dda31b4a6ab944
Andrew Lamoureux <andrew@vector35.com>
2016-12-01 10:41:12 -0500

    llvm_mc_tests: use armv7 vs arm arch in triplet

--- llvm_mc_tests/test-macos.sh
+++ llvm_mc_tests/test-macos.sh
@@ -4,13 +4,15 @@
 # clang -S -c will produce .s (assembler) but it's difficult to specify target
 # llc will produce hello.s
 
+set -x
+
 clang -S -c -target i386-none-none hello.c -o x86.s
 clang -S -c -target i386-none-none hello.c -masm=intel -o x86_intel.s
 clang -S -c -target x86_64-none-none hello.c -o x86_64.s
 clang -S -c -target x86_64-none-none hello.c -masm=intel -o x86_64_intel.s
 clang -S -c -target arm-none-none-eabi hello.c -o arm.s
 clang -S -c -target arm64-none-none-eabi hello.c -o arm64.s
-clang -S -c -target thumb-none-none-eabi hello.c -o thumb.s
+clang -S -c -target thumbv7-none-none-eabi hello.c -o thumb.s
 clang -S -c -target powerpc-none-none hello.c -o ppc.s
 clang -S -c -target powerpc64-none-none hello.c -o ppc64.s
 clang -S -c -target mips-pc-none hello.c -o mips.s
@@ -22,7 +24,7 @@ llvm-mc-mp-3.7 -triple=x86_64-none-none -assemble x86_64.s -o x86-64.o -filetype
 llvm-mc-mp-3.7 -x86-asm-syntax=intel -triple=x86_64-none-none -assemble x86_64_intel.s -o x86-64_intel.o -filetype=obj
 llvm-mc-mp-3.7 -triple=arm-none-none-eabi -assemble arm.s -o arm.o -filetype=obj
 llvm-mc-mp-3.7 -triple=arm64-none-none-eabi -assemble arm64.s -o arm64.o -filetype=obj
-llvm-mc-mp-3.7 -triple=thumb-none-none-eabi -assemble thumb.s -o thumb.o -filetype=obj
+llvm-mc-mp-3.7 -triple=thumbv7-none-none-eabi -assemble thumb.s -o thumb.o -filetype=obj
 llvm-mc-mp-3.7 -triple=powerpc-none-none -assemble ppc.s -o ppc.o -filetype=obj
 llvm-mc-mp-3.7 -triple=powerpc64-none-none -assemble ppc64.s -o ppc64.o -filetype=obj
 llvm-mc-mp-3.7 -triple=mips-pc-none -assemble mips.s -o mips.o -filetype=obj
