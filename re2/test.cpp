9926bffb6cc2f216e4ed9cbd0f83ab4fbe254014
Andrew Lamoureux <andrew@vector35.com>
2017-03-29 15:42:34 -0400

    more re2 tests

--- re2/test.cpp
+++ re2/test.cpp
@@ -11,10 +11,17 @@
 int main(int ac, char **av)
 {
 // Example: successful match
-	printf("%d", RE2::FullMatch("hello", "h.*o"));
+	std::string m0, m1, m2;
+	printf("%d\n", RE2::FullMatch("hellerooskio", "(h([ler]+)(.*)o)", &m0, &m1, &m2));
+	printf("m0: %s\n", m0.c_str());
+	printf("m1: %s\n", m1.c_str());
+	printf("m2: %s\n", m2.c_str());
+
+	printf("%d\n", RE2::PartialMatch("hellerooskio", "(e*)", &m0));
+	printf("m0: %s\n", m0.c_str());
 //
 // Example: unsuccessful match (requires full match):
-	printf("%d", RE2::FullMatch("hello", "JFW(*EFJ(#FJIF(QWFYYFY&&&&(**e"));
+	//printf("%d", RE2::FullMatch("hello", "JFW(*EFJ(#FJIF(QWFYYFY&&&&(**e"));
 
 	return 0;
 }
