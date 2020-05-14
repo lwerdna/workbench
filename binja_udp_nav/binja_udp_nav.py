fc0ca47821e9ed8be05aa9ffdccfb42e9a37ebdf
Andrew Lamoureux <andrew@vector35.com>
2020-05-14 15:25:16 -0400

    add try/except around module import and int conversion

--- binja_udp_nav/binja_udp_nav.py
+++ binja_udp_nav/binja_udp_nav.py
@@ -4,7 +4,11 @@ import socket
 import threading
 
 import binaryninja
-from binaryninjaui import DockHandler
+headless = False
+try:
+	from binaryninjaui import DockHandler
+except ModuleNotFoundError:
+	headless = True
 
 def worker(bv):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,17 +17,23 @@ def worker(bv):
 	while True:
 		print('udp_nav: calling sock.recvfrom()')
 		data, addr = sock.recvfrom(1024)
-		data = data.decode('utf-8')
-		address = int(data, 16)
 		print("udp_nav: received ", data)
+
+		try:
+			data = data.decode('utf-8')
+			address = int(data, 16)
+		except Exception:
+			continue
 
 		dh = DockHandler.getActiveDockHandler()
 		vf = dh.getViewFrame()
 		cv = vf.getCurrentView()
+		print('udp_nav: bv.navigate(\'%s\', 0x%X)' % (cv, address))
 		bv.navigate(cv, address)
 
 def go(bv):
 	threading.Thread(target=worker, args=(bv,)).start()
 
-from binaryninja.plugin import PluginCommand
-PluginCommand.register("udp nav", "udp nav", go)	
+if not headless:
+	from binaryninja.plugin import PluginCommand
+	PluginCommand.register("udp nav", "udp nav", go)	
