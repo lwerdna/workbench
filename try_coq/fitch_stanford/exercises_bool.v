ea401686039ba52e3f9d196c537779c71008a7dd
Andrew Lamoureux <andrew@vector35.com>
2020-06-02 14:28:53 -0400

    try_coq: fitch 04 01

--- try_coq/fitch_stanford/exercises_bool.v
+++ try_coq/fitch_stanford/exercises_bool.v
@@ -12,6 +12,23 @@ Proof.
     reflexivity.
 Qed.
 
+Require Import Bool.
+
+(* http://intrologic.stanford.edu/exercises/exercise_04_01.html *)
+Theorem fitch_04_01 : forall p q r: bool,
+  p=true /\ q=true /\ (((andb p q)=true) -> r=true) -> r=true.
+Proof.
+  intros p q r tmp.
+  destruct tmp as [p_holds tmp].
+  destruct tmp as [q_holds tmp].
+  destruct tmp as [pq_implies_r].
+  rewrite p_holds. 
+  rewrite q_holds.
+  simpl.
+  reflexivity.
+  reflexivity.
+Qed.
+
 (* http://intrologic.stanford.edu/exercises/exercise_04_02.html *)
 Theorem fitch_04_02 : forall p q : bool,
   (andb p q)=true -> (orb p q)=true.
