f15145aacba4e0ef8789e1d875b5fb2dc1d6d4aa
Andrew Lamoureux <andrew@vector35.com>
2021-03-23 16:17:32 -0400

    flgofishing: script to call scrape.py on all pages

---
+++ scrape_flgofishing/all.sh
@@ -0,0 +1,52 @@
+#/bin/sh
+
+# panhandle
+./scrape.py https://www.floridagofishing.com/reefs/nc-reefs-wakulla-county.html > nc-reefs-wakulla-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/nc-reefs-wakulla-county.html > nc-reefs-wakulla-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/nw-reefs-gulf-county.html > nw-reefs-gulf-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/nw-reefs-bay-county.html > nw-reefs-bay-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/nw-reefs-walton-county.html > nw-reefs-walton-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/nw-reefs-okaloosa-county.html > nw-reefs-okaloosa-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/nw-reefs-santa-rosa-county.html > nw-reefs-santa-rosa-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/nw-reefs-escambia-county.html > nw-reefs-escambia-county.csv
+
+# central west
+./scrape.py https://www.floridagofishing.com/reefs/cw-reefs-pasco-county.html > cw-reefs-pasco-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/cw-reefs-pinellas-county.html > cw-reefs-pinellas-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/cw-reefs-hillsborough-county.html > cw-reefs-hillsborough-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/cw-reefs-manatee-county.html > cw-reefs-manatee-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/cw-reefs-sarasota-county.html > cw-reefs-sarasota-county.csv
+
+# southwest
+./scrape.py https://www.floridagofishing.com/reefs/sw-reefs-charlotte-county.html > sw-reefs-charlotte-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/sw-reefs-lee-county.html > sw-reefs-lee-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/sw-reefs-collier-county.html > sw-reefs-collier-county.csv
+
+# big bend
+./scrape.py https://www.floridagofishing.com/reefs/cw-reefs-hernando-county.html > cw-reefs-hernando-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/cw-reefs-citrus-county.html > cw-reefs-citrus-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/nc-reefs-dixie-county.html > nc-reefs-dixie-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/nc-reefs-levy-county.html > nc-reefs-levy-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/nc-reefs-taylor-county.html > nc-reefs-taylor-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/nc-reefs-jefferson-county.html > nc-reefs-jefferson-county.csv
+
+# central
+./scrape.py https://www.floridagofishing.com/reefs/cw-reefs-pasco-county.html > cw-reefs-pasco-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/nw-reefs-gulf-county.html > nw-reefs-gulf-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/keys-reefs-lower-monroe-county.html > keys-reefs-lower-monroe-county.csv
+
+# keys
+./scrape.py https://www.floridagofishing.com/reefs/keys-reefs-upper-monroe-county.html > keys-reefs-upper-monroe-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/keys-reefs-middle-monroe-county.html > keys-reefs-middle-monroe-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/keys-reefs-lower-monroe-county.html > keys-reefs-lower-monroe-county.csv
+
+# central east
+./scrape.py https://www.floridagofishing.com/reefs/ce-reefs-brevard-county.html > ce-reefs-brevard-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/ce-reefs-indian-river-county.html > ce-reefs-indian-river-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/ce-reefs-st-lucie-county.html > ce-reefs-st-lucie-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/ce-reefs-martin-county.html > ce-reefs-martin-county.csv
+
+# southest
+./scrape.py https://www.floridagofishing.com/reefs/se-reefs-palm-beach-county.html > se-reefs-palm-beach-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/se-reefs-broward-county.html > se-reefs-broward-county.csv
+./scrape.py https://www.floridagofishing.com/reefs/se-reefs-miami-dade-county.html > se-reefs-miami-dade-county.csv
