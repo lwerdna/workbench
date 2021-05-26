7eb58fca7a672027ddb99da3f2632997b91aa65e
Andrew Lamoureux <andrew@vector35.com>
2021-05-26 18:12:36 -0400

    get_debian_sos: also get debug symbols

--- get_debian_sos/get_libgcc_so.py
+++ get_debian_sos/get_libgcc_so.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# download debian shared objects, separate debug symbols, and unstrip them
+
 import os, sys, re
 
 from subprocess import Popen, PIPE
@@ -36,15 +38,28 @@ if __name__ == '__main__':
 			url = 'http://http.us.debian.org/debian/pool/main/g/gcc-6/' + fname
 			os.system('wget '+url)
 
+			fname2 = fname.replace('-gdb', '')
+			if os.path.exists(fname2): continue
+			url = 'http://http.us.debian.org/debian/pool/main/g/gcc-6/' + fname2
+			os.system('wget '+url)
+
 		for fname in fnames:
 			os.system('ar -vx '+fname)
 			os.system('tar -xvf data.tar.xz')
 
 	elif sys.argv[1] == 'harvest':
-		for fpath in get_files('.debug'):
+		for fpath in get_files('.so.1'):
 			(output, _) = shellout(['file', fpath])
 			info = (output.split(': ')[1]).split(', ')
 			assert 'ELF' in info[0] and ('MSB' in info[0] or 'LSB' in info[0])
+			assert info[4].startswith('BuildID')
 			endian = 'big' if 'MSB' in info[0] else 'little'
-			print('%s endian=%s arch=%s, %s' % (fpath, endian, info[1], info[2]))
+			build_id = info[4].split('=')[1]
+
+			debug_path = './usr/lib/debug/.build-id/%s/%s.debug' % (build_id[0:2], build_id[2:])
+			assert os.path.exists(debug_path)
+			print('%s build_id=%s' % (fpath, build_id))
+			print('\tendian=%s' % endian)
+			print('\tarch=%s, %s' % (info[1], info[2]))
+			print('\tdebug: %s' % debug_path)
 
