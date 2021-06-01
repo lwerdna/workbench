0444e9064c7fe77b2b8dc9b4a4be5fb5bf89eb8b
Andrew Lamoureux <andrew@vector35.com>
2021-06-01 15:53:54 -0400

    Haskell 99 questions: problems 11-20

--- 99questions/prob_11_20.hs
+++ 99questions/prob_11_20.hs
@@ -29,5 +29,73 @@ encodeModifiedTest (n, a) = Multiple n a
 encodeModified :: Eq a => [a] -> [EmElem a]
 encodeModified x = map encodeModifiedTest (encode x)
 
+-- prob12 --
+decodeModified :: [EmElem a] -> [a]
+decodeModified [] = []
+decodeModified (Single x : xs) = [x] ++ decodeModified xs
+decodeModified (Multiple n x : xs) = replicate n x ++ decodeModified xs
+
+-- prob13 --
+-- do not use helper --
+encodeDirect :: Eq a => [a] -> [EmElem a]
+encodeDirect [] = []
+encodeDirect x
+    | (l == 1) = [Single (head x)] ++ encodeDirect (tail x)
+    | otherwise = [Multiple l (head x)] ++ encodeDirect (drop l x)
+    where l = length (takeWhile (\k -> k==head x) x)
+
+-- prob14 --
+dupli :: Eq a => [a] -> [a]
+dupli [] = []
+dupli x = [head x, head x] ++ dupli (tail x)
+
+-- prob15 --
+repli :: Eq a => [a] -> Int -> [a]
+repli [] n = []
+repli x n = replicate n (head x) ++ repli (tail x) n
+
+-- prob16 --
+dropEvery :: [a] -> Int -> [a]
+dropEvery x n
+    | length x < n = x
+    | otherwise = take (n-1) x ++ dropEvery (drop n x) n
+
+-- prob17 --
+myTake :: Int -> [a] -> [a]
+myTake 0 x = []
+myTake n (x:xs) = [x] ++ myTake (n-1) xs
+
+myDrop :: Int -> [a] -> [a]
+myDrop 0 x = x
+myDrop n (x:xs) = myDrop (n-1) xs
+
+split :: [a] -> Int -> ([a], [a])
+split x n = (myTake n x, myDrop n x)
+
+-- prob18 --
+-- this one is 1-indexed --
+slice2 :: [a] -> Int -> Int -> [a]
+slice2 x i j = take (j-i+1) (drop (i-1) x)
+
+-- prob19 --
+rotate :: [a] -> Int -> [a]
+rotate x n
+    | n > 0 = drop n x ++ take n x
+    | n < 0 = drop n' x ++ take n' x
+    where n' = length x + n
+
+-- prob20 --
+removeAt :: Int -> [a] -> (a, [a])
+removeAt n x = (x !! (n-1), take (n-1) x ++ drop n x)
+
 main = do
-    print (encodeModified "aaaabccaadeeee") 
+    print (encodeModified "aaaabccaadeeee")
+    print (decodeModified [Multiple 4 'a',Single 'b',Multiple 2 'c', Multiple 2 'a',Single 'd',Multiple 4 'e'])
+    print (encodeDirect "aaaabccaadeeee")
+    print (dupli [1,2,3])
+    print (repli "abc" 3)
+    print (split "abcdefghijk" 3)
+    print (slice2 ['a','b','c','d','e','f','g','h','i','k'] 3 7)
+    print (rotate ['a','b','c','d','e','f','g','h'] (-2))
+    print (rotate ['a','b','c','d','e','f','g','h'] 3)
+    print (removeAt 2 "abcd")
