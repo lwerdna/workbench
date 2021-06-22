5748ab47bed9c06901859a215361bc6bbcb89b36
Andrew Lamoureux <andrew@vector35.com>
2021-06-22 10:30:39 -0400

    gdb_types: add README, screenshot

--- gdb_types/README.md
+++ gdb_types/README.md
@@ -1,5 +1,13 @@
+Extract type information from DWARF by gdb script.
+
+![](./screenshot.png)
+
+unsorted notes:
+
+```
 gdb -q ./linux/aarch64/libc.so -ex 'maint print symbols' -ex 'q' > /tmp/out.txt
 gdb -q ./linux/aarch64/libc.so -ex 'info types' -ex 'q' > /tmp/out.txt
 gdb -q ./linux/aarch64/libc.so -ex 'info functions' -ex 'q' > /tmp/out.txt
 gdb -q ./linux/aarch64/libc.so -x onetype.py > foo.dot
+```
 
--- knights_tour/drawit/go.py
+++ knights_tour/drawit/go.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 
-import itertools
-
-def moves(square):
-    (x,y) = square
+def moves(x, y):
     candidates = [ (x+1,y-2), (x+2,y-1), (x+1,y+2), (x+2,y+1),
                    (x-1,y-2), (x-2,y-1), (x-1,y+2), (x-2,y+1) ]
     return [c for c in candidates if c[0]>=0 and c[0]<8 and c[1]>=0 and c[1]<8]
     
-def sqr2str(square):
-    return 'abcdefgh'[square[0]] + str(square[1]+1)
+def sqr2str(x, y):
+    return 'abcdefgh'[x] + str(y+1)
 
 print('digraph graphname {')
-for src in [(x,y) for x in range(8) for y in range(8)]:
-    for dst in moves(src):
-        print('\t%s -> %s;' % (sqr2str(src), sqr2str(dst)))
+for (x,y) in [(a,b) for a in range(8) for b in range(8)]:
+    for (x2,y2) in moves(x, y):
+        print('\t%s -> %s;' % (sqr2str(x,y), sqr2str(x2,y2)))
 print('}')
 
