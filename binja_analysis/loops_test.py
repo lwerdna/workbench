b7cd4bf5f9c4eabdcacf1a7bd7908c07784fd09c
Andrew Lamoureux <andrew@vector35.com>
2021-10-11 21:42:38 -0400

    compare loop detection vs. networkx cycle detection

---
+++ binja_analysis/loops_test.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+# test loop detection by comparison against NetworkX (graph theory library)
+
+import sys
+from binaryninja import BinaryViewType
+import networkx as nx
+from loops import get_loops
+
+# return a string identifier for a given basic block node
+def bbid(bb):
+    return 'b%d' % bb.index
+
+# convert a Binary Ninja CFG to a NetworkX directed graph
+def binja_func_to_networkx_graph(func):
+    G = nx.DiGraph()
+
+    for src in func.basic_blocks:
+        G.add_node(bbid(src))
+
+        for dst in [edge.target for edge in src.outgoing_edges]:
+            G.add_node(bbid(dst))
+            G.add_edge(bbid(src), bbid(dst))
+        
+    return G
+
+if __name__ == '__main__':
+    fpath = './tests' if not sys.argv[1:] else sys.argv[1]
+
+    bv = BinaryViewType.get_view_of_file(fpath)
+    if not bv:
+        raise Exception('binary ninja didnt return analysis on -%s-' % fpath)
+    bv.update_analysis_and_wait()
+
+    for func in bv.functions:
+        print('-------- analyzing function %s --------' % func.name)
+
+        # get binja's answer
+        #
+        answer_binja = set()
+        for (i,loop) in enumerate(get_loops(func)):
+            answer_binja.update([bbid(bb) for bb in loop])
+        print('binja says:', answer_binja)
+
+        # get networkx's answer
+        #
+        G = binja_func_to_networkx_graph(func)
+        answer_nx = set()
+        for cycle in nx.simple_cycles(G):
+            answer_nx.update(cycle)
+        print('networkx says:', answer_nx)
+
+        # compare answers
+        #
+        if answer_binja == answer_nx:
+            print('PASS!')
+        else:
+            raise Exception('FAIL!')
