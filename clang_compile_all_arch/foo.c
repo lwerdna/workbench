02537d37f61e2db46efc6f604bf64c8f8fa5200b
Andrew Lamoureux <andrew@vector35.com>
2021-11-07 09:26:45 -0500

    clang_compile_all_arch: test with ctypes

--- clang_compile_all_arch/foo.c
+++ clang_compile_all_arch/foo.c
@@ -2,3 +2,8 @@ int add(int a, int b)
 {
     return a+b;
 }
+
+int collatz_next(n)
+{
+	return 3*n + 1;
+}
---
+++ clang_compile_all_arch/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import ctypes
+import platform
+
+identifier = platform.system() + '-' + platform.machine()
+
+lookup = {
+    'Windows-x86': 'foo-win-x86.dll',
+    'Windows-x86_64': 'foo-win-x64.dll',
+    'Darwin-x86_64': 'foo-macos-x64.dylib',
+    'Linux-x86': 'foo-linux-x86.so',
+    'Linux-x86_64': 'foo-linux-x64.so'
+}
+
+fname = lookup[identifier]
+
+sharedlib = ctypes.cdll.LoadLibrary(fname)
+
+collatz_next = sharedlib.collatz_next
+collatz_next.argtypes = [ctypes.c_int]
+collatz_next.restype = ctypes.c_int
+
+print('collatz_next(5) = %d' % collatz_next(5))
+print('collatz_next(6) = %d' % collatz_next(6))
+print('collatz_next(7) = %d' % collatz_next(7))
+print('collatz_next(8) = %d' % collatz_next(8))
