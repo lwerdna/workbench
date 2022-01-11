bd80e9ce642b0f11db4d693b3ba4d6e16ad30d72
Andrew Lamoureux <andrew@vector35.com>
2022-01-10 23:15:01 -0500

    try_svg_js: draw-binomial-distribution improvements

--- try_svg_js/draw-binomial-distribution/test.js
+++ try_svg_js/draw-binomial-distribution/test.js
@@ -11,10 +11,21 @@ function body_loaded()
 
 function binary(x, width)
 {
-	var result = ''
-	for(var i=0; i<width; ++i)
-		result = ((x>>i)&1) + result;
-	return result;
+	return Array.from(new Array(width), (value, index) => (x>>index)&1);
+}
+
+function binary_string(x, width)
+{
+	return binary(x, width).map(k => k.toString()).join("")
+}
+
+function set_text(name, value)
+{
+	text = xml_doc.getElementById(name);
+	console.assert(text.nodeName == "text");
+	tspan = text.children[0];
+	console.assert(tspan.nodeName == "tspan");
+	tspan.textContent = value;
 }
 
 function svg_loaded()
@@ -28,14 +39,14 @@ function svg_loaded()
 	for(var i=0; i<8; i++)
 	{
 		/* get tspan i */
-		text = xml_doc.getElementById("text_" + binary(i, 3));
+		text = xml_doc.getElementById("text_" + binary_string(i, 3));
 		console.assert(text.nodeName == "text");
 		tspan = text.children[0]
 		console.assert(tspan.nodeName == "tspan");
 		tspans[i] = tspan;
 
 		/* get rect i */
-		rect = xml_doc.getElementById("rect_" + binary(i, 3));
+		rect = xml_doc.getElementById("rect_" + binary_string(i, 3));
 		console.assert(rect.nodeName == "rect");
 		rects[i] = rect;
 	}
@@ -57,10 +68,10 @@ function probability(events, p)
 	var result = 1;
 	for (var i=0; i<events.length; ++i)
 	{
-		if(events[i] == '0')
-			result *= (1-p);
-		else
+		if(events[i])
 			result *= p;
+		else
+			result *= (1-p);
 	}
 	return result;
 }
@@ -69,6 +80,16 @@ function slider_changed(evt)
 {
 	p = slider.value;
 	p_output.textContent = "p: " + p
+
+	for (var i=0; i<2; ++i)
+	{
+		set_text('text_'+i, probability(binary(i, 1), p).toFixed(3));
+	}
+
+	for (var i=0; i<4; ++i)
+	{
+		set_text('text_'+binary_string(i, 2), probability(binary(i, 2), p).toFixed(3));
+	}
 
 	for (var i=0; i<8; ++i)
 	{
