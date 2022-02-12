87926880409ee01d8f8da5e9af46a1d752c91f2d
Andrew Lamoureux <andrew@vector35.com>
2022-02-12 00:42:03 -0500

    z3-examples: one-big-conjunction.py still mystifies

--- z3-examples/one-big-conjunction.py
+++ z3-examples/one-big-conjunction.py
@@ -9,6 +9,7 @@
 # https://stackoverflow.com/questions/24711329/which-is-better-practice-in-smt-to-add-multiple-assertions-or-single-and/24712801#24712801
 
 import time
+import cProfile
 import itertools
 
 from z3 import *
@@ -23,8 +24,7 @@ def get_n_rooks_solver_A():
             name = f'v_{row}_{col}'
             vdict[name] = z3.Bool(name)
 
-
-    s = Solver()
+    s = Solver(logFile='/tmp/a.log')
 
     # verbose checks for attacks
     # v_0_0 -> Not(Or(v_0_1, v_0_2, v_0_3, ...
@@ -40,7 +40,7 @@ def get_n_rooks_solver_A():
 
             #print('%s -> Not(Or(%s))' % (p, ', '.join([str(q) for q in qs])))
             s.add(Implies(p, Not(Or(*qs))))
-   
+
     # verbose check for k variables being true
     # warning! generates nCr(n**2, n) clauses
     vset = set(vdict.values())
@@ -62,9 +62,6 @@ def get_n_rooks_solver_B():
             name = f'v_{row}_{col}'
             vdict[name] = z3.Bool(name)
 
-
-    s = Solver()
-
     # verbose checks for attacks
     # v_0_0 -> Not(Or(v_0_1, v_0_2, v_0_3, ...
     for row in range(n):
@@ -79,7 +76,7 @@ def get_n_rooks_solver_B():
 
             #print('%s -> Not(Or(%s))' % (p, ', '.join([str(q) for q in qs])))
             clauses.append(Implies(p, Not(Or(*qs))))
-   
+
     # verbose check for k variables being true
     # warning! generates nCr(n**2, n) clauses
     vset = set(vdict.values())
@@ -88,19 +85,14 @@ def get_n_rooks_solver_B():
         disjuncts.append(z3.And(*list(ingroup)))
     clauses.append(Or(*disjuncts))
 
-    s.add(And(*clauses))
-
-    return s
+    s2 = Solver(logFile='/tmp/b.log')
+    s2.add(And(*clauses))
+    return s2
 
 def print_solution(m):
-    def piece(b):
-        return 'R' if b else '-'
-
     name2var = {v.name(): v for v in m.decls()}
     name2val = {name: m[var] for (name, var) in name2var.items()}
-    name2char = {name: piece(val) for (name, val) in name2val.items()}
-
-    m = s.model()
+    name2char = {name: ('R' if val else '-') for (name, val) in name2val.items()}
 
     for row in range(5):
         for col in range(5):
@@ -114,16 +106,37 @@ def dump_smt_lib(solver, fpath):
         fp.write('\n(check-sat)\n(get-model)\n')
 
 if __name__ == '__main__':
+    t0 = time.time()
     s = get_n_rooks_solver_A()
+    with open('/tmp/a.assertions', 'w') as fp:
+        for c in s.assertions():
+            fp.write(str(c) + '\n')
+    print('get_n_rooks_solver_A(): took %fs' % (time.time() - t0))
     dump_smt_lib(s, '/tmp/a.smt2')
     t0 = time.time()
-    assert s.check() == z3.sat
+    cProfile.run('s.check()')
+    print("elapsed: %fs" % (time.time() - t0))
+    #assert s.check() == z3.sat
+    with open('/tmp/a.stats', 'w') as fp:
+        fp.write(str(s.statistics()))    
     print('solution via adding clauses to the solver took %fs' % (time.time() - t0))
     print_solution(s.model())
 
+    #s.reset()
+    #del s
+
+    t0 = time.time()
     s = get_n_rooks_solver_B()
+    with open('/tmp/b.assertions', 'w') as fp:
+        for c in s.assertions():
+            fp.write(str(c) + '\n')
+    print('get_n_rooks_solver_B(): took %fs' % (time.time() - t0))
     dump_smt_lib(s, '/tmp/b.smt2')
     t0 = time.time()
-    assert s.check() == z3.sat
+    cProfile.run('s.check()')
+    print("elapsed: %fs" % (time.time() - t0))
+    #assert s.check() == z3.sat
+    with open('/tmp/b.stats', 'w') as fp:
+        fp.write(str(s.statistics()))    
     print('solution via one big conjunction took %fs' % (time.time() - t0))
     print_solution(s.model())
