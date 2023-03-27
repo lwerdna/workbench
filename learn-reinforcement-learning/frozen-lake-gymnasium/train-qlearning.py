0bf6bbeff65deea83b676ed22e92778936a5495f
Andrew Lamoureux <andrew@vector35.com>
2023-03-27 11:41:03 -0400

    RL: -1 frznlake hole wrapper, try exponential decay epsilon

--- learn-reinforcement-learning/frozen-lake-gymnasium/train-qlearning.py
+++ learn-reinforcement-learning/frozen-lake-gymnasium/train-qlearning.py
@@ -48,6 +48,8 @@ class QLearner():
 
         return '\n'.join(lines)
 
+# epsilon is the probability a random action will be taken
+# it should start high (initial exploration) and decrease over time (move towards exploitation)
 class DecayedEpsilonGreedy():
     def __init__(self, epsilon_min, epsilon_max):
         self.epsilon_min = epsilon_min
@@ -55,26 +57,63 @@ class DecayedEpsilonGreedy():
 
     def choose_action(self, qtable, state, progress):
         # reminder: 0,1,2,3 is left,down,right,up
-        epsilon = self.epsilon_max - progress*(self.epsilon_max - self.epsilon_min)
+
+        # linear decay
+        if 0:
+            epsilon = self.epsilon_max - progress*(self.epsilon_max - self.epsilon_min)
+        # exponential decay
+        if 1:
+            # at 100% progress, I want there to be only a P chance of a random action
+            P = .01
+            k = 1 / P + 1
+            epsilon = 1 / (k*progress + 1)
+
+        if epsilon < self.epsilon_min:
+            epsilon = self.epsilon_min
+
 
         actions = list(range(len(qtable[0])))
 
+        log = f'progress={100*progress}%, P(choose randomly)={round(epsilon, 2)} '
+
         if random.random() < epsilon:
             result = random.choice(actions)
-            print(f'choosing random action returns {result}')
+            log += f'choice=RANDOM action returns {result}'
         else:
             qbest = max(qtable[state])
             actions = [i for (i,q) in enumerate(qtable[state]) if q==qbest]
             result = random.choice(actions)
-            print(f'choosing best action from {actions} which have q={qbest} returns {result}')
+            log += f'choice=BEST from {actions} with q={qbest} returns {result}'
+
+        print(log)
 
         return result
+
+# wrapper that makes stepping into a hole -1 (default is 0)
+class HoleIsNegativeOne(gym.core.RewardWrapper):
+    def __init__(self, env: gym.Env):
+        super().__init__(env)
+
+    def reward(self, reward):
+        env = self.env
+
+        row, col = env.s // env.ncol, env.s % env.ncol
+
+        match env.desc[row][col]:
+            case b"S": return 0 # start
+            case b"F": return 0 # frozen
+            case b"H": return -1 # hole
+            case b"G": return 1 # goal
+
+        breakpoint()
 
 if __name__ == '__main__':
     ql = QLearner(.7, .99, 16, 4)
     deg = DecayedEpsilonGreedy(.01, 1)
 
     env = gym.make('FrozenLake-v1', map_name="4x4", is_slippery=False, render_mode='human')
+
+    env = HoleIsNegativeOne(env)
 
     # convert an observation to a state id (here they're 1:1)
     def obs_to_state(obs):
@@ -84,15 +123,16 @@ if __name__ == '__main__':
     observation, info = env.reset()
     state = obs_to_state(observation)
 
-    max_steps = 10000
+    max_steps = 1000
     for step in range(max_steps):
         print('')
         print('--- step ---')
-        action = deg.choose_action(ql.qtable, state, step / max_steps)
+        progress = step / max_steps if step < max_steps else 1
+        action = deg.choose_action(ql.qtable, state, progress)
 
         observation, reward, terminated, truncated, info = env.step(action)
         state_next = obs_to_state(observation)
-    
+
         ql.update(state, action, reward, state_next)
         print(ql)
 
