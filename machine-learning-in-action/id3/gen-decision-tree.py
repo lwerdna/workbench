600995debe603e17f4483b84cffa581a0a26ba0d
Andrew Lamoureux <andrew@vector35.com>
2023-05-06 00:18:57 -0400

    finish ID3 decision tree generating

--- machine-learning-in-action/id3/gen-decision-tree.py
+++ machine-learning-in-action/id3/gen-decision-tree.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-# example 3.4 using decision trees to predict lense type
-
 import os, sys
 import pprint
 
@@ -9,49 +7,98 @@ import entropy
 
 column_names = ['age', 'prescr', 'astigmatic', 'tear_rate', 'lenses']
 
+#------------------------------------------------------------------------------
+# data table helpers
+#------------------------------------------------------------------------------
+
 def entropy_of_data_set(data):
     classes = [row[-1] for row in data]
     return entropy.entropy_A(classes)
 
-def data_copy(data):
-    data2 = []
-    for row in data:
-        data2.append(list(row))
-    return data2
-
 def split(data, column):
-    result = []
+    result = {}
     for attr_value in {row[column] for row in data}:
         subset = [list(row) for row in data if row[column] == attr_value]
         for row in subset:
             row[column] = None
-        result.append(subset)
+        result[attr_value] = subset
     return result
+
+#------------------------------------------------------------------------------
+# decision tree builder
+#------------------------------------------------------------------------------
 
 def info_gain(data, column):
     before = entropy_of_data_set(data)
-    after = sum(entropy_of_data_set(x) for x in split(data, column))
+
+    after = 0
+    for subset in split(data, column).values():
+        proportion = len(subset) / len(data)
+        after += proportion * entropy_of_data_set(subset)
+
     return before - after
 
-data = []
-for line in open('./lenses.dat').readlines():
-    age, prescr, astigmatic, tear_rate, lenses = line.strip().split('\t')
-    data.append([age, prescr, astigmatic, tear_rate, lenses])
+def build_decision(data):
+    assert len(data) > 0
 
-pprint.pprint(data)
+    if len(set([row[-1] for row in data]))==1:
+        return {'type':'class', 'result':data[0][-1]}
 
-gains = {column_names[col]: info_gain(data, col) for col in range(4)}
-pprint.pprint(gains)
-best = max(gains, key=lambda k: gains[k])
-print(best)
+    best_col, best_gain = None, None
+    for col in range(4):
+        gain = info_gain(data, col)
+        print(f'column {col} ("{column_names[col]}") has information gain {gain}')
+        if best_col == None or gain > best_gain:
+            best_col, best_gain = col, gain
 
+    result = {'type':'decision', 'attribute':column_names[best_col], 'children':{}}
+    for attr_val, subdata in split(data, best_col).items():
+        result['children'][attr_val] = build_decision(subdata)
 
-#for result in split(data, 0):
-#    print('----')
-#    pprint.pprint(result)
+    return result
 
-#print(entropy_of_data_set(data))
+#------------------------------------------------------------------------------
+# DOT/graphviz funcitons
+#------------------------------------------------------------------------------
+def tree_dot_vertices(tree):
+    result = []
+    match tree['type']:
+        case 'decision':
+            result.append(f'{id(tree)} [shape="diamond" label="{tree["attribute"]}"];')
+            for c in tree['children'].values():
+                result.extend(tree_dot_vertices(c))
+        case 'class':
+            result.append(f'{id(tree)} [label="{tree["result"]}"];')
+    return result
 
+def tree_dot_edges(tree):
+    result = []
+    if tree['type'] == 'decision':
+        for attr_val, child in tree['children'].items():
+            result.append(f'{id(tree)} -> {id(child)} [label="{attr_val}"];')
+            result.extend(tree_dot_edges(child))
+    return result
 
+def tree_to_dot(tree):
+    print('digraph G {')
+    print('// nodes')
+    print('\n'.join(tree_dot_vertices(tree)))
+    print('// edges')
+    print('\n'.join(tree_dot_edges(tree)))
+    print('}')
 
+#------------------------------------------------------------------------------
+# main
+#------------------------------------------------------------------------------
 
+if __name__ == '__main__':
+    data = []
+    for line in open('./lenses.dat').readlines():
+        age, prescr, astigmatic, tear_rate, lenses = line.strip().split('\t')
+        data.append([age, prescr, astigmatic, tear_rate, lenses])
+
+    tree = build_decision(data)
+
+    pprint.pprint(tree)
+
+    tree_to_dot(tree)
