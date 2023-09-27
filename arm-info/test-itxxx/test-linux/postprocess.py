ee19b17676e7093e0ff3be3ab89bbd34972e4f9b
Andrew Lamoureux <andrew@vector35.com>
2023-09-27 17:09:57 -0400

    Move itXXX tests to arm-info

--- arm-info/test-itxxx/test-linux/postprocess.py
+++ arm-info/test-itxxx/test-linux/postprocess.py
@@ -4,8 +4,13 @@ guts = None
 with open('hello', 'rb') as fp:
     guts = fp.read()
 
+# move r8, r8
 before = 6 * b'\xc0\x46'
 
+# be bf       ittt lt
+# af f3 00 80 nop.w
+# af f3 00 80 nop.w
+# f8 e7       b top
 after = \
 b'\xbe\xbf' + \
 b'\xaf\xf3\x00\x80' + \
