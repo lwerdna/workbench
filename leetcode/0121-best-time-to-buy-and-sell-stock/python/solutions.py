36f067267e39f4bfd7168c8fdb313478db64977a
Andrew Lamoureux <andrew@vector35.com>
2023-10-20 16:20:06 -0400

    leetcode: add 0121-best-time-to-buy-and-sell-stock solutions

---
+++ leetcode/0121-best-time-to-buy-and-sell-stock/python/solutions.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+# Algorithm:
+# Scan right, tracking minimum amount encountered so far (best buy price to the left).
+# Treat each current price as a sell price, subtracting the best buy price, tracking the biggest difference.
+class Solution0:
+    def maxProfit(self, prices):
+        max_profit = 0
+        min_price = 10000
+        for p in prices:
+            min_price = min(min_price, p)
+            max_profit = max(max_profit, p - min_price)
+
+        return max_profit
+
+# Algorithm:
+# Scan left, tracking maximum amount encountered so far (best sell price to the right).
+# Treat each current price as a buy price, subtracting it from best sell price, tracking the biggest difference.
+class Solution1:
+    def maxProfit(self, prices):
+        max_profit = 0
+        max_price = -1
+        for p in prices[::-1]:
+            max_price = max(max_price, p)
+            max_profit = max(max_profit, max_price - p)
+
+        return max_profit
+
+import itertools
+
+# Algorithm:
+# Create lookup table of best sell price.
+# Scan right, subtracting current price (buy) from best sell price, recording best difference (profit).
+class Solution2:
+    def maxProfit(self, prices):
+        max_profit = 0
+
+        # [7,1,5,3,6,4] ->
+        # [7,6,6,6,6,4]
+        lookup = list(reversed(list(itertools.accumulate(reversed(prices), max))))
+
+        for i,p in enumerate(prices):
+            max_profit = max(max_profit, lookup[i] - p)
+
+        return max_profit
+
+if __name__ == '__main__':
+    for sol in [Solution0(), Solution1(), Solution2()]:
+        assert sol.maxProfit([7,1,5,3,6,4]) == 5
+        assert sol.maxProfit([7,6,4,3,1]) == 0
