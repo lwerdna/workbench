c4819015480d7c91a2118e69bbd8d7edf95fa6d0
Andrew Lamoureux <lwerdna@users.noreply.github.com>
2025-01-12 22:16:03 -0500

    115: detect vocab words from books

---
+++ 115-find-complicated-sentences/README.txt
@@ -0,0 +1,29 @@
+Crude experiments detecting vocab words from books.
+
+From 1984:
+
+**slatternly**
+
+> There was a strip of carpet on the floor, a picture or two on the walls, and a deep, slatternly arm-chair drawn up to the fireplace.
+
+**timorously**
+
+> His pale-grey eyes flitted timorously from face to face and turned quickly away again when he caught anyone’s eye.
+
+**desultorily**
+
+> They talked desultorily for some minutes, then, without apparent reason, a yell from the telescreen bade them be silent.
+
+**persiflage**
+
+> When he spoke of murder, suicide, venereal disease, amputated limbs, and altered faces, it was with a faint air of persiflage.
+
+**sententious**
+
+> ‘Thoughtcrime is a dreadful thing, old man,’ he said sententiously.
+
+**execrate**
+
+> The citizen of Oceania is not allowed to know anything of the tenets of the other two philosophies, but he is taught to execrate them as barbarous outrages upon morality and common sense.
+
+
---
+++ 115-find-complicated-sentences/go.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import nltk
+import pandas
+import wordfreq
+
+text = open('1984.txt').read()
+text = text.replace('\n', ' ')
+
+sentences = nltk.sent_tokenize(text)
+
+word2freq = {}
+word2sents = {}
+
+for sentence in sentences:
+    for word in nltk.word_tokenize(sentence):
+        freq = wordfreq.word_frequency(word, 'en')
+        if freq == 0:
+            continue
+        if not word.isalpha():
+            continue
+        word2freq[word] = wordfreq.word_frequency(word, 'en')
+        word2sents[word] = word2sents.get(word, []) + [sentence]
+
+for i, word in enumerate(sorted(word2freq, key=lambda w: word2freq[w])):
+    freq = word2freq[word]
+    print(f'"{word}" has frequence {freq}')
+    sentences = word2sents[word]
+    print(f'and appears in {len(sentences)}:')
+    for s in sentences:
+        print('  ' + s)
+    if i > 40:
+        break
+    print('----')
+
+sys.exit(0)
+
+def calc_score(sentence):
+    result = 0
+
+    words = nltk.word_tokenize(sentence)
+    for word in words:
+        result += wordfreq.word_frequency(word, 'en')
+
+    result /= len(words)
+
+    return result
+
+sent2score = {}
+for sentence in sentences:
+    sent2score[sentence] = calc_score(sentence)
+
+def find_outliers_iqr(data):
+    data = pandas.Series(data)
+    q1 = data.quantile(0.25)
+    q3 = data.quantile(0.75)
+    iqr = q3 - q1
+    lower_bound = q1 - 1.5 * iqr
+    upper_bound = q3 + 1.5 * iqr
+    outliers = data[(data < lower_bound) | (data > upper_bound)]
+    return outliers
+
+def find_rare_word(sentence):
+    words = nltk.word_tokenize(sentence)
+    words = [w for w in words if wordfreq.word_frequency(w, 'en') != 0]
+    freqs = {wordfreq.word_frequency(w, 'en'):w for w in words}
+
+    outliers = find_outliers_iqr(list(freqs))
+    if len(outliers) == 1:
+        outlier = outliers.values[0]
+        if outlier == min(freqs):
+            rare_word = freqs[outlier]
+            return rare_word
+
+for sentence in sentences:
+    #word = find_rare_word(sentence)
+    words = nltk.word_tokenize(sentence)
+    words = [w for w in words if wordfreq.word_frequency(w, 'en') != 0]
+    freqs = {wordfreq.word_frequency(w, 'en'):w for w in words}
+
+    if len(words) < 10:
+        continue
+
+    min_freq = min(freqs)
+    min_word = freqs[min_freq]
+
+    print('sentence: ' + sentence)
+    print('rareword: ' + min_word)
+
+#for i,sentence in enumerate(sorted(sentences, key=lambda x:sent2score[x])):
+#    print('> ' + sentence)
+#    score = calc_score(sentence)
+#    print('score:', score)
+#
+#    if i >= 100:
+#        break
