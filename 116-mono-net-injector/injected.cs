50330c6838df3d8dee8d2f888ae5a14c6e02b516
Andrew Lamoureux <lwerdna@users.noreply.github.com>
2025-01-17 15:24:15 -0500

    116: put long-running injected code in thread

--- 116-mono-net-injector/injected.cs
+++ 116-mono-net-injector/injected.cs
@@ -6,11 +6,33 @@ namespace Test
 {
 	public class Test
 	{
+		public static void Worker()
+		{
+			foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+			{
+				Console.WriteLine("STAGE2: assembly: {0}", assembly);
+
+				foreach (var type in assembly.GetTypes())
+				{
+					Console.WriteLine("STAGE2:   type: {0}", type);
+	
+					foreach (var meth in type.GetMethods())
+					{
+						Console.WriteLine("STAGE2:     method: {0}", meth);
+					}
+				}
+			}
+
+		}
+
 		public static void TestMethod()
 		{
 			int pid = System.Diagnostics.Process.GetCurrentProcess().Id;
 
 			Console.WriteLine("STAGE2: I'm inside pid {0}!", pid);
+
+	        Thread thread = new Thread(new ThreadStart(Worker));
+	        thread.Start();
 		}
 	}
 }
