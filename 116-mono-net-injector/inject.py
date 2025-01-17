de66e4b32e7534909613d35d9c8b701721154947
Andrew Lamoureux <lwerdna@users.noreply.github.com>
2025-01-17 14:57:22 -0500

    116: mono inject progress

--- 116-mono-net-injector/inject.py
+++ 116-mono-net-injector/inject.py
@@ -14,7 +14,7 @@ def gdb(gdbmi, command):
     return [x['payload'] for x in response if x['type']=='console']
 
 if __name__ == '__main__':
-    sopath = sys.argv[1]
+    assembly_path = sys.argv[1]
     pid = int(sys.argv[2])
 
     # change with:
@@ -26,19 +26,30 @@ if __name__ == '__main__':
                 print(f'WARNING: ptrace may be limited by {fpath}')
                 print(f'   HINT: echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope')
 
-    if not os.path.isabs(sopath):
-        sopath = os.path.abspath(sopath)
+    # check path to assembly
+    if not os.path.isabs(assembly_path):
+        assembly_path = os.path.abspath(assembly_path)
+    if not os.path.exists(assembly_path):
+        print(f'ERROR: {assembly_path} does not exist')
+        sys.exit(-1)
 
-    print(f'injecting {sopath} into pid {pid}')
+    # check path to stage1
+    stage1_path = os.path.join(os.getcwd(), 'stage1.so')
+    if not os.path.exists(stage1_path):
+        print(f'ERROR: {stage1_path} does not exist')
+        sys.exit(-1)
 
     gdbmi = GdbController()
     gdb(gdbmi, f'attach {pid}')
 
-    lines = gdb(gdbmi, f'call dlopen("{sopath}", 2)')
-    breakpoint()
+    # get stage1 to memory
+    lines = gdb(gdbmi, f'call dlopen("{stage1_path}", 2)')
     handle = int(re.search(r' 0x([a-fA-F0-9]+)', lines[-1]).group(1), 16)
     print(f'shared object loaded to: 0x{handle:X}')
 
-    gdb(gdbmi, f'call (void)load_assembly_call_method("/path/to/foo.dll", "FooNamespace", "FooClass", "FooMethod")')
+    # call stage1 with path to stage2 (the .net dll)
+    gdb(gdbmi, f'call (void)load_assembly_call_method("{assembly_path}", "FooNamespace", "FooClass", "FooMethod")')
+
+    # cleanup
     gdb(gdbmi, f'call dlclose(0x{handle:X})')
     gdb(gdbmi, f'detach')
--- 116-mono-net-injector/stage1.c
+++ 116-mono-net-injector/stage1.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #include <dlfcn.h>
 
@@ -17,7 +18,7 @@ void *mono_image_strerror;
 void *mono_object_get_class;
 void *mono_class_get_name;
 
-void *read_file_to_buffer(char *fpath)
+void read_file_to_buffer(char *fpath, void **data, uint32_t *data_len)
 {
     FILE *file;
     char *buffer;
@@ -27,7 +28,7 @@ void *read_file_to_buffer(char *fpath)
     file = fopen(fpath, "rb");
     if (file == NULL) {
         perror("Error opening file");
-        return NULL;
+        return;
     }
 
     fseek(file, 0, SEEK_END);
@@ -40,7 +41,7 @@ void *read_file_to_buffer(char *fpath)
     if (buffer == NULL) {
         perror("Error allocating memory");
         fclose(file);
-        return NULL;
+        return;
     }
 
     // Read the file into the buffer
@@ -50,7 +51,7 @@ void *read_file_to_buffer(char *fpath)
         perror("Error reading file");
         fclose(file);
         free(buffer);
-        return NULL;
+        return;
     }
 
     // Close the file and free the memory
@@ -58,7 +59,8 @@ void *read_file_to_buffer(char *fpath)
     fclose(file);
 
     printf("returning %p\n", buffer);
-    return buffer;
+    *data = buffer;
+    *data_len = file_size;
 }
 
 __attribute__ ((constructor))
@@ -81,18 +83,35 @@ void load_assembly_call_method(char *assembly_path, char *namespace, char *class
 
 	void *tmp;
 
+	//
 	// resolve needed symbols in mono
+	//
+
+	// MonoDomain *mono_get_root_domain (void)
 	void *(*mono_get_root_domain)(void) = dlsym(handle, "mono_get_root_domain");
 	if (mono_get_root_domain==NULL) { printf("ERROR: resolving symbol mono_get_root_domain\n"); return; }
 
 	tmp = dlsym(handle, "mono_thread_attach");
 	if (tmp==NULL) { printf("ERROR: resolving symbol\n"); return; }
-	tmp = dlsym(handle, "mono_image_open_from_data");
-	if (tmp==NULL) { printf("ERROR: resolving symbol\n"); return; }
-	tmp = dlsym(handle, "mono_assembly_load_from_full");
-	if (tmp==NULL) { printf("ERROR: resolving symbol\n"); return; }
-	tmp = dlsym(handle, "mono_assembly_get_image");
-	if (tmp==NULL) { printf("ERROR: resolving symbol\n"); return; }
+
+	// MonoImage *mono_image_open_from_data (char *data, uint32_t data_len,
+	//   mono_bool need_copy, MonoImageOpenStatus *status);
+	void *(*mono_image_open_from_data)(void *, uint32_t, uint32_t, uint32_t *) =
+		dlsym(handle, "mono_image_open_from_data");
+
+	if (mono_image_open_from_data==NULL) { printf("ERROR: resolving symbol mono_image_open_from_data\n"); return; }
+
+	// MonoAssembly* mono_assembly_load_from_full  (MonoImage *image, const char *fname,
+	//				MonoImageOpenStatus *status,
+	//				mono_bool refonly);
+	void *(*mono_assembly_load_from_full)(void *, void *, void *, uint32_t)	=
+		dlsym(handle, "mono_assembly_load_from_full");
+	if (mono_assembly_load_from_full==NULL) { printf("ERROR: resolving symbol\n"); return; }
+
+	// MonoImage    *mono_assembly_get_image  (MonoAssembly *assembly);
+	void *(*mono_assembly_get_image)(void *) = dlsym(handle, "mono_assembly_get_image");
+	if (mono_assembly_get_image==NULL) { printf("ERROR: resolving symbol\n"); return; }
+
 	tmp = dlsym(handle, "mono_class_from_name");
 	if (tmp==NULL) { printf("ERROR: resolving symbol\n"); return; }
 	tmp = dlsym(handle, "mono_class_get_method_from_name");
@@ -108,20 +127,31 @@ void load_assembly_call_method(char *assembly_path, char *namespace, char *class
 	tmp = dlsym(handle, "mono_class_get_name");
 	if (tmp==NULL) { printf("ERROR: resolving symbol\n"); return; }
 
-	// mono interaction strategy from: https://github.com/warbler/SharpMonoInjector
+	// mono interaction strategy:
+	// https://github.com/warbler/SharpMonoInjector
+	// https://journal.lunar.sh/2020/mono-dot-net-injection.html
 
-	// MonoDomain *mono_get_root_domain (void)
 	void *root_domain = mono_get_root_domain();
 	printf("root_domain: %p\n", root_domain);
 
-	// MonoImage *mono_image_open_from_data (char *data, uint32_t data_len, mono_bool need_copy, MonoImageOpenStatus *status);
-	void *buf = read_file_to_buffer(assembly_path);
-	printf("file read to: %p\n", buf);
+	void *data;
+	uint32_t data_len;
+	read_file_to_buffer(assembly_path, &data, &data_len);
+	printf("%d bytes from %s read to: %p\n", data_len, assembly_path, data);
+
 	int status;
+	void *image = mono_image_open_from_data(data, data_len, 1, &status);
+	printf("image: %p\n", image);
+
+	void *assembly = mono_assembly_load_from_full(image, assembly_path, &status, 1);
+	printf("assembly: %p\n", assembly);
+
+	void *image2 = mono_assembly_get_image(assembly);
+	printf("image2: %p\n", image2);
 
 	//void *raw_image = mono_image_open_from_data();
 	//void *image = mono_assembly_get_image(assembly);
 	//void *class = mono_class_from_name(image, namespace, classname);
 	//void *method = mono_class_get_method_from_name();
 	//mono_runtime_invoke()
-}	
+}
